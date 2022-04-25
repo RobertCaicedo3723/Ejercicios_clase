34c12cc6fc425bcb4013f518c7bdcd9a821074cd
RobertCaicedo3723 <robert2224513@correo.uis.edu.co>
2022-04-25 09:19:50 -0500

    Ejercicios

---
+++ programa_par.md
@@ -0,0 +1,6 @@
+# Ejercicio calcular par
+
+## Programa para calclular si un numero es par o impar
+
+### Diagrama de flujo
+![Diagrama de flujo](programa_par.png "Diagrama de flujo")
---
+++ programa_par.py
@@ -0,0 +1,16 @@
+print("""PROGRAMA PARA CALCULAR SI UN NUMERO ES PAR O IMPAR""")
+print()
+print("-----------------------------------")
+print("---- PROGRAMA PARA CALCULAR SI UN NUMERO ES PAR --------")
+print("-----------------------------------")
+
+# Input
+
+numero = int(input("Ingresa un número: "))
+
+# Procesing
+
+if numero % 2 == 0:
+    print("El número es par") # output
+else:
+    print("El número es impar") #output
